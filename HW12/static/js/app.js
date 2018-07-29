9d3470c8e322f3f31433e8b54e233cf0dabe43b9
TEAbraham <thomas.e.abraham@gmail.com>
2018-07-29 15:07:41 -0500

    HW12

--- HW12/static/js/app.js
+++ HW12/static/js/app.js
@@ -5,18 +5,18 @@
 // var tbody = d3.select("tbody");
 // var filterBtn = d3.select("#filter-btn");
 
-// // renderTable
-// function renderTable() {
-//     tableData.forEach(function(ufo){
-//         console.log(ufo);
-//         var row = tbody.append("tr");
-//         Object.entries(ufo).forEach(function([key, value]) {
-//           console.log(key, value);
-//           var cell = tbody.append("td");
-//           cell.text(value);
-//         });
-//       });
-//   }
+// renderTable
+function renderTable() {
+    filteredTable.forEach(function(ufo){
+        console.log(ufo);
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(function([key, value]) {
+          console.log(key, value);
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+  }
 
 // // render initial table
 // renderTable();
@@ -104,24 +104,24 @@ $filterBtn.addEventListener("click", handleFilterButtonClick);
 // Set filteredAddresses to dataSet initially
 var filteredTable = data;
 
-// renderTable renders the filteredAddresses to the tbody
-function renderTable() {
-  $tbody.innerHTML = "";
-  for (var i = 0; i < filteredTable.length; i++) {
-    // Get get the current address object and its fields
-    var address = filteredTable[i];
-    console.log(address)
-    var fields = Object.keys(address);
-    // Create a new row in the tbody, set the index to be i + startingIndex
-    var $row = $tbody.insertRow(i);
-    for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
-      var field = fields[j];
-      var $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
-    }
-  }
-}
+// // renderTable renders the filteredAddresses to the tbody
+// function renderTable() {
+//   $tbody.innerHTML = "";
+//   for (var i = 0; i < filteredTable.length; i++) {
+//     // Get get the current address object and its fields
+//     var address = filteredTable[i];
+//     console.log(address)
+//     var fields = Object.keys(address);
+//     // Create a new row in the tbody, set the index to be i + startingIndex
+//     var $row = $tbody.insertRow(i);
+//     for (var j = 0; j < fields.length; j++) {
+//       // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+//       var field = fields[j];
+//       var $cell = $row.insertCell(j);
+//       $cell.innerText = address[field];
+//     }
+//   }
+// }
 
 // Render the table for the first time on page load
 renderTable();
