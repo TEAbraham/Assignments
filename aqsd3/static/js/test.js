2a612a5fd22eeaf84b6ece0ca42d49ab656b2d84
TEAbraham <thomas.e.abraham@gmail.com>
2018-08-21 16:30:26 -0500

    hw check

--- aqsd3/static/js/test.js
+++ aqsd3/static/js/test.js
@@ -1,3 +1,10 @@
+var bdate = "20180601"
+var edate = "20180608"
+
+var pmurl = `https://cors-anywhere.herokuapp.com/https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=81102&bdate=${bdate}&edate=${edate}&state=17&county=031`
+var ozurl = `https://cors-anywhere.herokuapp.com/https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=44201&bdate=${bdate}&edate=${edate}&state=17&county=031`
+var courl = `https://cors-anywhere.herokuapp.com/https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=42101&bdate=${bdate}&edate=${edate}&state=17&county=031`
+
 // Set up our chart
 var svgWidth = 1011;
 var svgHeight = 777;
@@ -5,6 +12,8 @@ var margin = { top: 30, right: 40, bottom: 100, left: 100 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+var parseTime = d3.timeParse("%Y%m%dT%H%M-%S%L")
 
 
 // Create an SVG wrapper, append an svg that will hold our chart and shift the latter by left and top margins
@@ -16,24 +25,303 @@ var svg = d3.select(".chart")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// append an svg group
+
 var chart = svg.append("g");
 
-d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
 
-d3.csv("https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=81102&bdate=20180601&edate=20180608&state=17&county=031", function(error1, data1) {
-    d3.csv("https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=44201&bdate=20180601&edate=20180608&state=17&county=031", function(error2, data2) {
-        d3.csv("https://aqs.epa.gov/api/rawData?user=thomas.e.abraham@gmail.com&pw=khakiosprey52&format=AQCSV&param=42101&bdate=20180601&edate=20180608&state=17&county=031", function(error3, data3) {
+function go(bdate, edate){
+d3.csv(pmurl).then(data1 => {
+    d3.csv(ozurl).then(data2 => {
+        d3.csv(courl).then(data3 => {
+        
+            var pmdata = data1
+            var ozdata = data2
+            var codata = data3
+            var alldata = d3.merge([data1,data2,data3])
+           
+            // Initialize tooltip
+            var toolTip = d3.tip()
+                .attr("class", "tooltip")
+                .html(function(alldata) {
+                    var datetime = parseTime(alldata.datetime)
+                    var value = +alldata.value;
+                    var units = alldata.units;
+                    if (alldata.parameter==="81102"){return (`PM10 Measures ${value} Micrograms Per Cubic Meter<hr>${datetime}`)}
+                    else if (alldata.parameter==="44201"){return (`Ozone Measures ${value} Parts Per Million<hr>${datetime}`)}
+                    else {return (`CO Measures ${value} Parts Per Million<hr>${datetime}`)}
+              });
+        
+            // Create tooltip
+            chart.call(toolTip);
+        
+        
+            // define scale functions(range)
+            var xScale = d3.scaleTime().range([0, width]);
+            var yScale = d3.scaleLinear().range([height, 0]);
+        
+            // define axis functions
+            var xAxis = d3.axisBottom().scale(xScale);
+            var yAxis = d3.axisLeft().scale(yScale);
+        
+            // these variables store the min and max values in a column in data.csv
+            var xRan;
+            var yMin;
+            var yMax;
+        
+            // create a function to identify min, max values of a column in data.csv which in turn
+            // assigns the results to the variables created above
+            function findMinAndMax(data) {
+                xRan = d3.extent(data, function (d) { return parseTime(d["datetime"])});
+                yMin = d3.min(data, function (d) { return d["value"] * 0.8 });
+                yMax = d3.max(data, function (d) { return d["value"] * 1.2 });
+            };
+            
+            // set the default x-axis
+            var defaultAxisLabelX = "datetime"
+        
+            // set the default y-axis
+            var defaultAxisLabelY = "PM10"
+        
+            // call the findMinAndMax() on the default X Axis
+            findMinAndMax(alldata) 
+        
+            // set the domain of the axes
+            xScale.domain(xRan);
+            yScale.domain([yMin, yMax])
+            
+            // create chart
+            chart.selectAll("circle")
+                .data(alldata)
+                .enter()
+                .append("circle")
+                .attr("cx", function (d) {
+                    return xScale(parseTime(d["datetime"]));
+                })
+                .attr("cy", function (d) {
+                    return yScale(d["value"]);
+                })
+                .attr("r", 5)
+                .attr("fill", "#1D8348")
+                .attr("opacity", 0.75)
+                // display tooltip on click
+                .on("mouseover", function (d) {
+                    toolTip.show(d);
+                })
+                // hide tooltip on mouseout
+                .on("mouseout", function (d) {
+                    toolTip.hide(d);
+                })
+        
+        
+            // create x-axis
+            chart.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", `translate(0,${height})`)
+                .call(xAxis);
+        
+            // create y-axis
+            chart.append("g")
+                .attr("class", "y-axis")
+                .call(yAxis)
+        
+        
+            // add x-axis titles
+            chart.append("text")
+                .attr("transform", `translate(${width / 2},${height + 40})`)
+                // This axis label is active by default
+                .attr("class", "axis-text-x active")
+                .attr("data-axis-name", "datetime")
+                .text("Time");
+        
+            // chart.append("text")
+            //     .attr("transform", `translate(${width / 2},${height + 60})`)
+            //     // This axis label is active by default
+            //     .attr("class", "axis-text-x inactive")
+            //     .attr("data-axis-name", "medianIncome")
+            //     .text("Household Income (Median)");
+        
+            // chart.append("text")
+            //     .attr("transform", `translate(${width / 2},${height + 80})`)
+            //     // This axis label is active by default
+            //     .attr("class", "axis-text-x inactive")
+            //     .attr("data-axis-name", "ageDependency")
+            //     .text("Age Dependency Ratio");
+        
+        
+            // add y-axis titles 
+            chart.append("text")
+                .attr("transform", `translate(-40,${height / 2})rotate(270)`)
+                .attr("class", "axis-text-y active")
+                .attr("data-axis-name", 'PM10')
+                .text("PM10 Total 0-10um");
+        
+        
+            chart.append("text")
+                .attr("transform", `translate(-80,${height / 2})rotate(270)`)
+                .attr("class", "axis-text-y inactive")
+                .attr("data-axis-name", 'O3')
+                .text("Ozone");
+        
+        
+            chart.append("text")
+                .attr("transform", `translate(-60,${height / 2})rotate(270)`)
+                .attr("class", "axis-text-y inactive")
+                .attr("data-axis-name", 'CO')
+                .text("Carbon Monoxide");
+        
+            // active-inactive toggle x axis
+            function labelChangeX(clickedAxis) {
+                d3.selectAll(".axis-text-x")
+                    .filter(".active")
+                    .classed("active", false)
+                    .classed("inactive", true);
+        
+                clickedAxis.classed("inactive", false).classed("active", true);
+            }
+        
+            // active-inactive toggle y axis
+            function labelChangeY(clickedAxis) {
+                d3.selectAll(".axis-text-y")
+                    .filter(".active")
+                    .classed("active", false)
+                    .classed("inactive", true);
+        
+                clickedAxis.classed("inactive", false).classed("active", true);
+            }
+        
+            // on click events for the x-axis
+            d3.selectAll(".axis-text-x").on("click", function () {
+        
+                // assign the variable to the current axis
+                var clickedSelection = d3.select(this);
+                var isClickedSelectionInactive = clickedSelection.classed("inactive");
+                console.log("this axis is inactive", isClickedSelectionInactive)
+                var clickedAxis = clickedSelection.attr("data-axis-name");
+                console.log("current axis: ", clickedAxis);
+        
+                if (isClickedSelectionInactive) {
+                    currentAxisLabelX = clickedAxis;
+        
+                    findMinAndMax(currentAxisLabelY);
+        
+                    xScale.domain(xRan);
+        
+                    // create x-axis
+                    svg.select(".x-axis")
+                        .transition()
+                        .duration(1000)
+                        .ease(d3.easeLinear)
+                        .call(xAxis);
 
-            PM10=data1
-            O3=data2
-            CO=data3
+                    d3.selectAll("circle")
+                        .data(currentAxisLabelY)
+                        .enter()
+                        .append("circle")
+                        .transition()
+                        .duration(1000)
+                        .ease(d3.easeLinear)
+                        .on("start", function () {
+                            d3.select(this)
+                                .attr("opacity", 0.50)
+                                .attr("r", 10)
+        
+                        })
+                        .attr("cx", function (d) {
+                            return xScale(d["datetime"]);
+                        })
+                        .on("end", function () {
+                            d3.select(this)
+                                .transition()
+                                .duration(500)
+                                .attr("r", 5)
+                                .attr("fill", "navy")
+                                .attr("opacity", 0.75);
+                        })
+        
+        
+                    labelChangeX(clickedSelection);
+                }
+            });
+        
+            // On click events for the y-axis
+            d3.selectAll(".axis-text-y").on("click", function () {
+        
+                // assign the variable to the current axis
+                var clickedSelection = d3.select(this);
+                var isClickedSelectionInactive = clickedSelection.classed("inactive");
+                console.log("this axis is inactive", isClickedSelectionInactive)
+                var clickedAxis = clickedSelection.attr("data-axis-name");
+                console.log("current axis: ", clickedAxis);
+        
+                if (isClickedSelectionInactive) {
+                    
+                    currentAxisLabelY = clickedAxis;
 
-            console.log(PM10)
-            console.log(O3)
-            console.log(CO)
+                    if (currentAxisLabelY==='O3'){currentAxisLabelY=ozdata;}
+                    else if (currentAxisLabelY==='CO'){currentAxisLabelY=codata;}
+                    else if (currentAxisLabelY==='PM10'){currentAxisLabelY=pmdata;}
+                    else (currentAxisLabelY=alldata)
 
-        });
-    });
-});
+                    findMinAndMax(currentAxisLabelY);
+        
+                    yScale.domain([yMin, yMax]);
+        
+                    // create y-axis
+                    svg.select(".y-axis")
+                        .transition()
+                        .duration(1000)
+                        .ease(d3.easeLinear)
+                        .call(yAxis);
 
+
+                    d3.selectAll("circle")
+                        .transition()
+                        .duration(1000)
+                        .ease(d3.easeLinear)
+                        .on("start", function () {
+                            d3.select(this)
+                                .attr("opacity", 0.50)
+                                .attr("r", 10)
+        
+                        })
+                        .attr("cx", function (d) {
+                            return xScale(parseTime(d["datetime"]));
+                        })
+                        .attr("cy", function (d) {
+                            return yScale(d["value"]);
+                        })
+                        .on("end", function () {
+                            d3.select(this)
+                                .transition()
+                                .duration(500)
+                                .attr("r", 5)
+                                .attr("fill", function (d) {if (d["parameter"]==="81102"){return "#C70039"}
+                                                        else if (d["parameter"]==="42101"){return "#FF5733"}
+                                                        else {return "#FFC30F"}})
+                                .attr("opacity", 0.75);
+                        })
+        
+        
+                    labelChangeY(clickedSelection);
+        
+                }
+        
+            });
+
+
+        })
+    })
+}) // this works!
+};
+
+go();
+
+d3.select('#to').on('keyup', function() {
+    // prevent form submission
+    d3.event.stopPropagation();
+    d3.event.preventDefault();
+    // later, submit form?
+    bdate = d3.select('#from').node();
+    edate = d3.select(this).node();
+    go(bdate, edate);
+  });
